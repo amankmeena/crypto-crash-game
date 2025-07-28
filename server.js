52adde282061a9b4509b2ec89bd23bfeac080cc4
Aman Meena <amankmeena845@gmail.com>
2025-07-28 09:40:06 +0530

    feat: changed port config

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(session({
 }));
 const server = http.createServer(app);
 const io = socketIo(server);
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
